72de108bc3c1ce53c3546ee1dca99f3d423dea6c
Evgenia Gorbacheva <31763733+EvgeniaGorbacheva@users.noreply.github.com>
2024-07-16 15:03:13 +0200

    Implemented logic to generate podspec for development pod (#67)
    Co-authored-by: Evgenia Gorbacheva <evgenia.gorbacheva@appodeal.com>

--- fastlane/actions/podspec.rb
+++ fastlane/actions/podspec.rb
@@ -42,7 +42,14 @@ module Fastlane
           spec.license = { type: "Copyright", text: "Copyright #{Time.new.year}. Bidon Inc." }
           spec.author = { "Bidon Inc." => "https://http://bidon.org" }
           spec.platform = :ios, "12.0"
-          spec.source = { http: "https://s3-#{s3_region}.amazonaws.com/#{s3_bucket}/#{spec.name}/#{CGI.escape(params[:version])}/#{spec.name}.zip" }
+
+          if params[:is_development_pod]
+            spec.source = { git: "" }
+            spec.source_files     = 'Bidon/**/*.{h,m,swift}'
+          else
+            spec.source = { http: "https://s3-#{s3_region}.amazonaws.com/#{s3_bucket}/#{spec.name}/#{CGI.escape(params[:version])}/#{spec.name}.zip" }
+          end
+
           spec.swift_versions = ["4.0", "4.2", "5.0"]
           unless params[:is_adapter]
             spec.resource_bundles = { "BidonPrivacyInfo" => "#{spec.name}-#{CGI.escape(params[:version])}/Bidon.xcframework/ios-arm64/**/*.xcprivacy" }
@@ -189,6 +196,13 @@ module Fastlane
               UI.user_error!("No pod name given, pass using `name: 'MyPod'`") unless (value and not value.empty?)
             end
           ),
+          FastlaneCore::ConfigItem.new(
+            key: :is_development_pod,
+            description: "Is development pod",
+            default_value: false,
+            optional: true,
+            is_string: false
+          )
         ]
       end
 
