851625719f5636ae4bbb534eb7f9112bfb8907b8
Evgenia Gorbacheva <evgenia.gorbacheva@appodeal.com>
2024-10-18 15:04:55 +0200

    Added win/loss flag

--- Bidon/Networking/Request/Auction/AuctionRequest.swift
+++ Bidon/Networking/Request/Auction/AuctionRequest.swift
@@ -61,6 +61,7 @@ struct AuctionRequest: Request {
         let auctionConfigurationUid: String
         let pricefloor: Price
         let auctionTimeout: Float
+        var externalWinNotifications: Bool
         
         enum CodingKeys: String, CodingKey {
             case adUnits
@@ -72,6 +73,7 @@ struct AuctionRequest: Request {
             case auctionPricefloor
             case auctionTimeout
             case noBids
+            case externalWinNotifications
         }
         
         init(from decoder: Decoder) throws {
@@ -85,6 +87,7 @@ struct AuctionRequest: Request {
             pricefloor = try container.decode(Price.self, forKey: .auctionPricefloor)
             auctionTimeout = try container.decodeIfPresent(Float.self, forKey: .auctionTimeout) ?? Constants.Timeout.defaultAuctionTimeout
             noBids = try container.decodeIfPresent([AdUnitModel].self, forKey: .noBids)
+            externalWinNotifications = try container.decode(Bool.self, forKey: .externalWinNotifications)
         }
     }
 }
