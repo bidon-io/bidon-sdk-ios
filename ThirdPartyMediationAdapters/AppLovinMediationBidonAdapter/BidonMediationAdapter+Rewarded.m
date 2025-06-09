6a3e64e59aba8665cd58f1a95098c743730ac1ea
Evgenia Gorbacheva <evgenia.gorbacheva@appodeal.com>
2025-06-09 16:21:27 +0200

    Removed local extras reading

--- ThirdPartyMediationAdapters/AppLovinMediationBidonAdapter/BidonMediationAdapter+Rewarded.m
+++ ThirdPartyMediationAdapters/AppLovinMediationBidonAdapter/BidonMediationAdapter+Rewarded.m
@@ -76,12 +76,6 @@
         return;
     }
     
-    NSString *storedLocalExtrasKey = @"stored_local_extras";
-    id storedLocalExtras = [NSUserDefaults.standardUserDefaults valueForKey:storedLocalExtrasKey];
-    if ([storedLocalExtras isKindOfClass:[NSDictionary class]]) {
-        [self.interstitialAd setExtraValue:storedLocalExtras for:storedLocalExtrasKey];
-    }
-    
     NSLog(@"[BidonAdapter] [%@] Presenting ad, Placement ID: %@", self.rewardedAdUnitId, self.rewardedPlacementId);
     [self.rewardedAd showAdFrom:parameters.presentingViewController];
 }
