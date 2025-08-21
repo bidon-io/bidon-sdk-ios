34a82758e6d8427053632dfc690414ca96bfc665
Andrei Rudyk <andrei.rudyk@appodeal.com>
2025-08-21 11:17:35 +0200

    BDN-1031 Moloco adapter - added
    # Conflicts:
    #	Adapters/Adapters.xcodeproj/project.pbxproj
    #	Podfile.lock

---
+++ Adapters/BidonAdapterMoloco/Models/MolocoParameters.swift
@@ -0,0 +1,13 @@
+//
+//  MolocoParameters.swift
+//  BidonAdapterMoloco
+//
+//  Created by Andrei Rudyk on 19/08/2025.
+//
+
+import Foundation
+
+public struct MolocoParameters: Decodable {
+    var appId: String
+    var appKey: String
+}
